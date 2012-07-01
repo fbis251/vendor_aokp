7a9cf245227cea65c6a1d1bddf6f5192a352bba0
Fernando Barillas <fbis251@gmail.com>
2012-07-01 07:26:22 -0700

    Add Team Acid Goo information
    Change-Id: I3566531780653a810e2f08abc72b5401998ba3ec

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -1,6 +1,11 @@
 # Version information used on all builds
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=IMM76I BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
 
+# Team Acid's version
+TA_STABLE := 40
+TA_RELEASE := 05
+TA_VERSION := $(TA_STABLE).$(TA_RELEASE)
+
 # Rom Manager properties
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.rommanager.developerid=teamkang \
@@ -8,7 +13,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Goo updater app
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.goo.developerid=aokp \
-    ro.goo.rom=aokp \
-    ro.goo.version=18
-
+    ro.goo.developerid=TeamAcid \
+    ro.goo.rom=AOKP_sgs4gmtd \
+    ro.goo.version=$(TA_VERSION) \
+    ro.goo.board=$(PRODUCT_DEVICE)
