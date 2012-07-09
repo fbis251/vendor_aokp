72e9abf9c1f252b17d4ff0ee9d2f369656fa81fa
Fernando Barillas <fbis251@gmail.com>
2012-07-09 09:09:59 -0700

    Now using date as version number
    Change-Id: I77a147a7c965ac189fc3a3604f61be0a8d5962fb
    Signed-off-by: Fernando Barillas <fbis251@gmail.com>

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -2,7 +2,7 @@
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=IMM76I BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
 
 # Team Acid's version
-TA_VERSION := 1
+TA_VERSION := $(shell date +"%Y%m%d")
 
 # Rom Manager properties
 PRODUCT_PROPERTY_OVERRIDES += \
